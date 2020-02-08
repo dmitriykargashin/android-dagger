7bc4d25fa69a79a1347543e50666457b642cc950
dmitriikargashin <dmitriy.kargashin@gmail.com>
2020-02-08 07:48:31 -0500

    Refactoring for Dagger2

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -3,13 +3,16 @@ package com.example.android.dagger.di
 import android.content.Context
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.registration.RegistrationComponent
+import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
+import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 // Definition of a Dagger component that adds info from the StorageModule to the graph
 @Singleton
-@Component(modules = [StorageModule::class])
+@Component(modules = [StorageModule::class, AppSubcomponents::class])
 interface AppComponent {
 
 
@@ -20,8 +23,12 @@ interface AppComponent {
         fun create(@BindsInstance context: Context): AppComponent
     }
 
+    // Expose RegistrationComponent factory from the graph
+    fun registrationComponent(): RegistrationComponent.Factory
 
     // Classes that can be injected by this Component
-    fun inject(activity: RegistrationActivity)
+   /* fun inject(activity: RegistrationActivity)
+    fun inject(fragment: EnterDetailsFragment)
+    fun inject(fragment: TermsAndConditionsFragment)*/
     fun inject(activity: MainActivity)
 }
